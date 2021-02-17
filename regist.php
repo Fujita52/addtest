de85e8092e64fb3777f0d3b3479e3acb2654000b
Fujita52 <fjry522@gmail.com>
2021-02-17 21:21:30 +0900

    varsion4

--- regist.php
+++ regist.php
@@ -1,3 +1,7 @@
+<?php
+//error_reporting(E_ALL & ~E_NOTICE);
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -11,111 +15,108 @@
 
     <h1>アカウント登録画面</h1>
     <form action="regist_confirm.php" method="post">
-        <div>
-            <label>名前（姓）</label>
-            <input type="text" class="text" name="family_name" size="35" maxlength="10">
-        </div>
-        <div>
-            <label>名前（名）</label>
-            <input type="text" class="text" name="last_name" size="35" maxlength="10">
-        </div>
-        <div>
-            <label>カナ（姓）</label>
-            <input type="text" class="text" name="family_name_kana" size="35" maxlength="10" pattern="[ア-ン]*">
-        </div>
-        <div>
-            <label>カナ（名）</label>
-            <input type="text" class="text" name="last_name_kana" size="35" maxlength="10" pattern="[ア-ン]*">
-        </div>
-        <div>
-            <label>メールアドレス</label>
-            <input type="text" class="text" name="mail" size="35" maxlength="100">
-        </div>
-        <div>
-            <label>パスワード</label>
-            <input type="text" class="text" name="password" size="35" maxlength="10">
-        </div>
-        <div>
-            <label>性別</label>
+        <p>名前（姓）
+            <input type="text" class="text" name="family_name" size="35" maxlength="10" value="<?php if(!empty($_POST["family_name"])){
+            echo $_POST["family_name"];} ?>">
+        </p>
+        <p>名前（名）
+            <input type="text" class="text" name="last_name" size="35" maxlength="10" value="<?php if(isset($_POST["last_name"])){
+            echo $_POST["last_name"];} ?>">
+        </p>
+        <p>カナ（姓）
+            <input type="text" class="text" name="family_name_kana" size="35" maxlength="10" pattern="[ァ-ン]*" value="<?php if(isset($_POST["family_name_kana"])){
+            echo $_POST["family_name_kana"];} ?>">
+        </p>
+        <p>カナ（名）
+            <input type="text" class="text" name="last_name_kana" size="35" maxlength="10" pattern="[ァ-ン]*" value="<?php if(isset($_POST["last_name_kana"])){
+            echo $_POST["last_name_kana"];} ?>">
+        </p>
+        <p>メールアドレス
+            <input type="email" class="text" name="mail" size="35" maxlength="100" value="<?php if(isset($_POST["mail"])){
+            echo $_POST["mail"];} ?>">
+        </p>
+        <p>パスワード
+            <input type="text" class="text" name="password" size="35" maxlength="10" pattern=^[0-9A-Za-z]+$ value="<?php if(isset($_POST["password"])){
+            echo $_POST["password"];} ?>">
+        </p>
+        <p>性別
             <input type="radio" name="gender" value="0" checked="checked"> 男
             <input type="radio" name="gender" value="1">女
-        </div>
-        <div>
-            <label>郵便番号</label>
-            <input type="text" class="text" name="postal_code" size="8" maxlength="7">
-        </div>
-        <div>
-            <label>住所（都道府県）</label>
-            <select name="prefecture">
-                <option></option>
-                <option>北海道</option>
-                <option>青森県</option>
-                <option>岩手県</option>
-                <option>宮城県</option>
-                <option>秋田県</option>
-                <option>山形県</option>
-                <option>福島県</option>
-                <option>茨城県</option>
-                <option>栃木県</option>
-                <option>群馬県</option>
-                <option>埼玉県</option>
-                <option>千葉県</option>
-                <option>東京都</option>
-                <option>神奈川県</option>
-                <option>新潟県</option>
-                <option>富山県</option>
-                <option>石川県</option>
-                <option>福井県</option>
-                <option>山梨県</option>
-                <option>長野県</option>
-                <option>岐阜県</option>
-                <option>静岡県</option>
-                <option>愛知県</option>
-                <option>三重県</option>
-                <option>滋賀県</option>
-                <option>京都府</option>
-                <option>大阪府</option>
-                <option>兵庫県</option>
-                <option>奈良県</option>
-                <option>和歌山県</option>
-                <option>鳥取県</option>
-                <option>島根県</option>
-                <option>岡山県</option>
-                <option>広島県</option>
-                <option>山口県</option>
-                <option>徳島県</option>
-                <option>香川県</option>
-                <option>愛媛県</option>
-                <option>高知県</option>
-                <option>福岡県</option>
-                <option>佐賀県</option>
-                <option>長崎県</option>
-                <option>熊本県</option>
-                <option>大分県</option>
-                <option>宮崎県</option>
-                <option>鹿児島県</option>
-                <option>沖縄県</option>
+        </p>
+        <p>郵便番号
+            <input type="number" name="postal_code" size="8" maxlength="7" value="<?php if(isset($_POST["postal_code"])){
+            echo $_POST["postal_code"];} ?>">
+        </p>
+        <p>住所（都道府県）
+
+
+        <p><select name="prefecture">
+                <option value="disabled" style="display:none;" <?php if(empty($_POST['prefecture'])) echo 'selected'; ?>>選択してください</option>
+                <option value="1" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='1'?'selected':'';?>>北海道</option>
+                <option value="2" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='2'?'selected':'';?>>青森県</option>
+                <option value="3" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='3'?'selected':'';?>>岩手県</option>
+                <option value="4" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='4'?'selected':'';?>>宮城県</option>
+                <option value="5" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='5'?'selected':'';?>>秋田県</option>
+                <option value="6" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='6'?'selected':'';?>>山形県</option>
+                <option value="7" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='7'?'selected':'';?>>福島県</option>
+                <option value="8" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='8'?'selected':'';?>>茨城県</option>
+                <option value="9" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='9'?'selected':'';?>>栃木県</option>
+                <option value="10" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='10'?'selected':'';?>>群馬県</option>
+                <option value="11" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='11'?'selected':'';?>>埼玉県</option>
+                <option value="12" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='12'?'selected':'';?>>千葉県</option>
+                <option value="13" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='13'?'selected':'';?>>東京都</option>
+                <option value="14" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='14'?'selected':'';?>>神奈川県</option>
+                <option value="15" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='15'?'selected':'';?>>新潟県</option>
+                <option value="16" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='16'?'selected':'';?>>富山県</option>
+                <option value="17" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='17'?'selected':'';?>>石川県</option>
+                <option value="18" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='18'?'selected':'';?>>福井県</option>
+                <option value="19" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='19'?'selected':'';?>>山梨県</option>
+                <option value="20" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='20'?'selected':'';?>>長野県</option>
+                <option value="21" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='21'?'selected':'';?>>岐阜県</option>
+                <option value="22" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='22'?'selected':'';?>>静岡県</option>
+                <option value="23" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='23'?'selected':'';?>>愛知県</option>
+                <option value="24" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='24'?'selected':'';?>>三重県</option>
+                <option value="25" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='25'?'selected':'';?>>滋賀県</option>
+                <option value="26" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='26'?'selected':'';?>>京都府</option>
+                <option value="27" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='27'?'selected':'';?>>大阪府</option>
+                <option value="28" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='28'?'selected':'';?>>兵庫県</option>
+                <option value="29" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='29'?'selected':'';?>>奈良県</option>
+                <option value="30" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='30'?'selected':'';?>>和歌山県</option>
+                <option value="31" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='31'?'selected':'';?>>鳥取県</option>
+                <option value="32" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='32'?'selected':'';?>>島根県</option>
+                <option value="33" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='33'?'selected':'';?>>岡山県</option>
+                <option value="34" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='34'?'selected':'';?>>広島県</option>
+                <option value="35" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='35'?'selected':'';?>>山口県</option>
+                <option value="36" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='36'?'selected':'';?>>徳島県</option>
+                <option value="37" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='37'?'selected':'';?>>香川県</option>
+                <option value="38" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='38'?'selected':'';?>>愛媛県</option>
+                <option value="39" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='39'?'selected':'';?>>高知県</option>
+                <option value="40" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='40'?'selected':'';?>>福岡県</option>
+                <option value="41" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='41'?'selected':'';?>>佐賀県</option>
+                <option value="42" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='42'?'selected':'';?>>長崎県</option>
+                <option value="43" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='43'?'selected':'';?>>熊本県</option>
+                <option value="44" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='44'?'selected':'';?>>大分県</option>
+                <option value="45" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='45'?'selected':'';?>>宮崎県</option>
+                <option value="46" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='46'?'selected':'';?>>鹿児島県</option>
+                <option value="47" <?php echo array_key_exists('prefecture',$_POST)&& $_POST['prefecture']=='47'?'selected':'';?>>沖縄県</option>
             </select>
-        </div>
-        <div>
-            <label>住所（市区町村）</label>
-            <input type="text" class="text" name="address_1" size="35" maxlength="10">
-        </div>
-        <div>
-            <label>住所（番地）</label>
-            <input type="text" class="text" name="address_2" size="35" maxlength="100">
-        </div>
-        <div>
-            <label>アカウント権限</label>
-            <select name="account">
+        </p>
+
+        <p>住所（市区町村）
+            <input type="text" class="text" name="address_1" size="35" maxlength="10" value="<?php if(isset($_POST["address_1"])){
+            echo $_POST["address_1"];} ?>">
+        </p>
+        <p>住所（番地）
+            <input type="text" class="text" name="address_2" size="35" maxlength="100" value="<?php if(isset($_POST["address_2"])){
+            echo $_POST["address_2"];} ?>">
+        </p>
+        <p>アカウント権限
+            <select name="authority">
                 <option value="0" selected>一般</option>
                 <option value="1">管理者</option>
             </select>
-        </div>
-        <div>
-            <input type="submit" value="確認する" class="submit">
-        </div>
-
+        </p>
+        <input type="submit" value="確認する" class="submit">
     </form>
 
 </body>
--- regist_confirm.php
+++ regist_confirm.php
@@ -13,36 +13,52 @@
 
     <div class="confirm">
 
-        <p>名前（姓）
-            <?php echo $_POST['family_name']; 
+        <p>名前（姓）:
+            <?php echo $_POST['family_name']."<br>"; 
+            if(empty($_POST['family_name'])){
+                echo '<font color="red">名前（姓）が未入力です</font>';
+            }
             ?>
         </p>
 
-        <p>名前（名）
-            <?php echo $_POST['last_name']; 
-            ?>
+        <p>名前（名）:
+            <?php echo $_POST['last_name']."<br>"; 
+            if(empty($_POST['last_name'])){
+                 echo '<font color="red">名前（名）が未入力です</font>';
+            }?>
         </p>
 
-        <p>カナ（姓）
-            <?php echo $_POST['family_name_kana']; ?>
+        <p>カナ（姓）:
+            <?php echo $_POST['family_name_kana']."<br>"; 
+            if(empty($_POST['family_name_kana'])){
+             echo '<font color="red">カナ（姓）が未入力です</font>';
+            }?>
         </p>
 
-        <p>カナ（名）
-            <?php echo $_POST['last_name_kana']; ?>
+        <p>カナ（名）:
+            <?php echo $_POST['last_name_kana']."<br>";
+            if(empty($_POST['last_name_kana'])){
+              echo '<font color="red">カナ（名）が未入力です</font>'; 
+            }?>
         </p>
-        <p>メールアドレス
-            <?php echo $_POST['mail']; ?>
+        <p>メールアドレス:
+            <?php echo $_POST['mail']."<br>";
+             if(empty($_POST['mail'])){
+                 echo '<font color="red">メールアドレスが未入力です</font>';
+             }?>
         </p>
-        <p>パスワード
+        <p>パスワード:
             <?php
             $str=$_POST['password'];
             for($i = 0;$i<strlen($str)
                 ;$i++){
                 echo "●";
             };
-            ?>
+             if(empty($_POST['password'])){
+               echo '<font color="red">パスワードが未入力です</font>';
+             }?>
         </p>
-        <p>性別
+        <p>性別:
             <?php 
             $gender=$_POST['gender']; 
             switch($gender){
@@ -55,22 +71,179 @@
             }
             ?>
         </p>
-        <p>郵便番号
-            <?php echo $_POST['postal_code']; ?>
+        <p>郵便番号:
+            <?php echo $_POST['postal_code']."<br>";
+             if(empty($_POST['postal_code'])){
+                 echo '<font color="red">郵便番号が未入力です</font>';
+             }?>
         </p>
         <p>住所（都道府県）
-            <?php echo $_POST['prefecture']; ?>
+            <?php  $prefecture=$_POST['prefecture']; 
+            switch($prefecture){
+                case 1:
+                    echo "北海道";
+                    break;
+                case 2:
+                    echo "青森";
+                    break; 
+                case 3:
+                    echo "岩手県";
+                    break;
+                case 4:
+                    echo "宮城県";
+                    break;
+                case 5:
+                    echo "秋田県";
+                    break;
+                case 6:
+                    echo "山形県";
+                    break;
+                case 7:
+                    echo "福島県";
+                    break;
+                case 8:
+                    echo "茨城県";
+                    break;
+                case 9:
+                    echo "栃木県";
+                    break;
+                case 10:
+                    echo "群馬県";
+                    break;
+                case 11:
+                    echo "埼玉県";
+                    break;
+                case 12:
+                    echo "千葉県";
+                    break;
+                case 13:
+                    echo "東京都";
+                    break;
+                case 14:
+                    echo "神奈川県";
+                    break;
+                case 15:
+                    echo "新潟県";
+                    break;
+                case 16:
+                    echo "富山県";
+                    break;
+                case 17:
+                    echo "石川県";
+                    break;
+                case 18:
+                    echo "福井県";
+                    break;
+                case 19:
+                    echo "山梨県";
+                    break;
+                case 20:
+                    echo "長野県";
+                    break;
+                case 21:
+                    echo "岐阜県";
+                    break;
+                case 22:
+                    echo "静岡県";
+                    break;
+                case 23:
+                    echo "愛知県";
+                    break;
+                case 24:
+                    echo "三重県";
+                    break;
+                case 25:
+                    echo "滋賀県";
+                    break;
+                case 26:
+                    echo "京都府";
+                    break;
+                case 27:
+                    echo "大阪府";
+                    break;
+                case 28:
+                    echo "兵庫県";
+                    break;
+                case 29:
+                    echo "奈良県";
+                    break;
+                case 30:
+                    echo "和歌山県";
+                    break;
+                case 31:
+                    echo "鳥取県";
+                    break;
+                case 32:
+                    echo "島根県";
+                    break;
+                case 33:
+                    echo "岡山県";
+                    break;
+                case 34:
+                    echo "広島県";
+                    break;
+                case 35:
+                    echo "山口県";
+                    break;
+                case 36:
+                    echo "徳島県";
+                    break;
+                case 37:
+                    echo "香川県";
+                    break;
+                case 38:
+                    echo "愛媛県";
+                    break;
+                case 39:
+                    echo "高知県";
+                    break;
+                case 40:
+                    echo "福岡県";
+                    break;
+                case 41:
+                    echo "佐賀県";
+                    break;
+                case 42:
+                    echo "長崎県";
+                    break;
+                case 43:
+                    echo "熊本県";
+                    break;
+                case 44:
+                    echo "大分県";
+                    break;
+                case 45:
+                    echo "宮崎県";
+                    break;
+                case 46:
+                    echo "鹿児島県";
+                    break;
+                case 47:
+                    echo "沖縄県";
+                    break;
+            }
+             if(empty($_POST['prefecture'])){
+           echo '<font color="red">住所（都道府県）が未入力です</font>';
+             }
+            ?>
         </p>
-        <p>住所（市区町村）
-            <?php echo $_POST['address_1']; ?>
+        <p>住所（市区町村）:
+            <?php echo $_POST['address_1']."<br>";
+             if(empty($_POST['address_1'])){
+              echo '<font color="red">住所（市区町村）が未入力です</font>';
+             }
+            ?>
         </p>
-        <p>住所（番地）
-            <?php echo $_POST['address_2']; ?>
+        <p>住所（番地）:
+            <?php echo $_POST['address_2']."<br>";
+            if(empty($_POST['address_2'])){
+               echo '<font color="red">住所（番地）が未入力です</font>';
+            }
+            ?>
         </p>
-        <p>アカウント権限
-            <?php 
-                $account=$_POST['account'];
-            switch($account){
+        <p>アカウント権限:
+            <?php $authority=$_POST['authority'];
+            switch($authority){
                 case 0:
                     echo "一般";
                     break;
@@ -78,10 +251,41 @@
                     echo "管理者";
                     break;
             }
-    ?>
+            ?>
         </p>
-    </div>
 
+        <form action="regist_complete.php" method="post">
+            <input type="submit" value="登録する" class="button">
+            <input type="hidden" value="<?php echo $_POST['family_name']; ?>" name="family_name">
+            <input type="hidden" value="<?php echo $_POST['last_name']; ?>" name="last_name">
+            <input type="hidden" value="<?php echo $_POST['family_name_kana']; ?>" name="family_name_kana">
+            <input type="hidden" value="<?php echo $_POST['last_name_kana']; ?>" name="last_name_kana">
+            <input type="hidden" value="<?php echo $_POST['mail']; ?>" name="mail">
+            <input type="hidden" value="<?php echo $_POST['password']; ?>" name="password">
+            <input type="hidden" value="<?php echo $_POST['gender']; ?>" name="gender">
+            <input type="hidden" value="<?php echo $_POST['postal_code']; ?>" name="postal_code">
+            <input type="hidden" value="<?php echo $_POST['prefecture']; ?>" name="prefecture">
+            <input type="hidden" value="<?php echo $_POST['address_1']; ?>" name="address_1">
+            <input type="hidden" value="<?php echo $_POST['address_2']; ?>" name="address_2">
+            <input type="hidden" value="<?php echo $_POST['authority']; ?>" name="authority">
+        </form>
+
+        <form action="regist.php" method="post">
+            <input type="submit" value="前に戻る" class="button">
+            <input type="hidden" value="<?php echo $_POST['family_name']; ?>" name="family_name">
+            <input type="hidden" value="<?php echo $_POST['last_name']; ?>" name="last_name">
+            <input type="hidden" value="<?php echo $_POST['family_name_kana']; ?>" name="family_name_kana">
+            <input type="hidden" value="<?php echo $_POST['last_name_kana']; ?>" name="last_name_kana">
+            <input type="hidden" value="<?php echo $_POST['mail']; ?>" name="mail">
+            <input type="hidden" value="<?php echo $_POST['password']; ?>" name="password">
+            <input type="hidden" value="<?php echo $_POST['gender']; ?>" name="gender">
+            <input type="hidden" value="<?php echo $_POST['postal_code']; ?>" name="postal_code">
+            <input type="hidden" value="<?php echo $_POST['prefecture']; ?>" name="prefecture">
+            <input type="hidden" value="<?php echo $_POST['address_1']; ?>" name="address_1">
+            <input type="hidden" value="<?php echo $_POST['address_2']; ?>" name="address_2">
+            <input type="hidden" value="<?php echo $_POST['authority']; ?>" name="authority">
+        </form>
+    </div>
 
 </body>
 
